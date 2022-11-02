46f72f12fdb6916a2b4f532c81a9491f1dfc9203
hathai25 <phamhuyhathai@gmail.com>
2022-11-02 16:59:13 +0700

    Set home scene

--- src/main/java/edu/hust/tu/projecti/InputNameController.java
+++ src/main/java/edu/hust/tu/projecti/InputNameController.java
@@ -2,7 +2,14 @@ package edu.hust.tu.projecti;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 import java.security.interfaces.RSAKey;
 import java.sql.*;
 
@@ -13,11 +20,13 @@ public class InputNameController {
     private Connection connection = null;
     private PreparedStatement statement = null;
     private ResultSet resultSet = null;
-    private String dbURL = "jdbc:mysql://localhost:3306/",
+    private String dbURL = "jdbc:mysql://localhost:3306/projecti",
             userName= "root",
             password= "";
     @FXML
-    protected void onClickConfirmName() {
+    protected void onClickConfirmName(ActionEvent event) {
+        Stage stage;
+        Scene scene;
         sUserName = tfUserName.getText();
         String sqlQuery = "INSERT INTO ProjectI.Users " +
                 "(Name, Score, `Date`) " +
@@ -31,10 +40,16 @@ public class InputNameController {
             statement = (PreparedStatement) connection.prepareStatement(sqlQuery);
             statement.setString(1, sUserName);
             userID = statement.executeUpdate();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Home-view.fxml"));
+            scene = new Scene(loader.load());
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        }catch (SQLException e) {} catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        }catch (SQLException e) {}
-
-       try{
+        try{
            connection = DriverManager.getConnection(dbURL,
                    userName,
                    password);
