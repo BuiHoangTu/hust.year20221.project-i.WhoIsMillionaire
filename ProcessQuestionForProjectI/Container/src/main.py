1e766d00ed2ffcdcfb146b25242a5ea5db376cd2
Bùi Hoàng Tú <bhtu.work@gmail.com>
2022-11-15 12:15:21 +0700

    excel to database

---
+++ ProcessQuestionForProjectI/Container/src/main.py
@@ -0,0 +1,89 @@
+import os
+
+import pandas as pd
+
+import mysql
+import mysql.connector
+from mysql.connector import Error as SQLError
+
+import random
+
+
+
+
+
+
+#get connection, cursor
+projectIDb = None
+
+try:
+    projectIDb = mysql.connector.connect(
+        host= "localhost",
+        user= "root",
+        port= 3306,
+        database= "ProjectI"
+    )   
+    print("DB ready\n")
+    cursor = projectIDb.cursor()
+except SQLError as e:
+    print("DB Error " + e)
+cursor = projectIDb.cursor(prepared= True)
+
+"""
+query = "Select * from Questions"
+try:
+    cursor.execute(query)
+    questions = cursor.fetchall()
+    for question in questions:
+        print(question)
+except SQLError as e:
+    print("DB Error " + e)
+ """
+
+
+## fuction region
+def sqlQMap(char : int):
+    if (char == 'A') : return 0
+    if (char == 'B') : return 1
+    if (char == 'C') : return 2
+    if (char == 'D') : return 3
+
+def sqlInsert(Question : str, AList : list[int], RIndex : int):
+    query = "INSERT INTO ProjectI.Questions(Question, RightAnswer, WrongAnswer1, WrongAnswer2, WrongAnswer3, QLevel)VALUES(%s, %s, %s, %s, %s, %s);"
+    selections = [0,1,2,3]
+    selections.remove(RIndex)
+    
+    selection = random.choice(selections)
+    selections.remove(selection)
+    WA1 = selection
+    
+    selection = random.choice(selections)
+    selections.remove(selection)
+    WA2 = selection
+
+    selection = random.choice(selections)
+    selections.remove(selection)
+    WA3 = selection
+
+    rparams = (Question,AList[RIndex],AList[WA1], AList[WA2], AList[WA3], str(random.choice([5,10,15])))
+    cursor.execute(query, rparams)
+#####################################END REGION#####################################
+
+
+
+
+#read pdf
+reader = pd.read_excel("Book.xlsx")
+reader.reset_index()
+for index, row in reader.iterrows(): #index is column number; row is each row
+    sqlQ = row[0]
+    aA = row[1]
+    aB = row[2]
+    aC = row[3]
+    aD = row[4]
+    sqlTrue = sqlQMap(row[5])
+    sqlInsert(sqlQ,[aA,aB,aC,aD],sqlTrue)
+projectIDb.commit()
+
+
+
