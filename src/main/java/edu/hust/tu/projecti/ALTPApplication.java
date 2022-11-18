0107959ff0b4599c1b85155a23ff2c9eea033907
Bùi Hoàng Tú <bhtu.work@gmail.com>
2022-11-18 09:45:10 +0700

    Use singleton in database connection

--- src/main/java/edu/hust/tu/projecti/ALTPApplication.java
+++ src/main/java/edu/hust/tu/projecti/ALTPApplication.java
@@ -6,11 +6,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ALTPApplication extends Application {
+    /**
+     * Database connect properties
+     * @deprecated
+     * These properties are no longer recommended to establish
+     * connection to database. They would become private in
+     * future version.
+     * <p> Use {@link package.class#getConnection()} instead
+     */
+    @Deprecated
     public static final String dbURL = "jdbc:mysql://localhost:3306/ProjectI",
             userName= "root",
             password= "";
+    private static volatile Connection connection = null;
+        public Connection getConnection(){
+            if(connection == null){
+                try{
+                    connection = DriverManager.getConnection(
+                            dbURL,
+                            userName,
+                            password
+                    );
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            return connection;
+        }
+
     @Override
     public void start(Stage stage){
         FXMLLoader fxmlLoader = new FXMLLoader(ALTPApplication.class.getResource("InputName-view.fxml"));
