fee91472349f15cb89a0f59a398c491b50dec9e4
Bùi Hoàng Tú <bhtu.work@gmail.com>
2022-12-07 08:08:23 +0700

    handel all questions, refactor vars' names

--- src/main/java/edu/hust/tu/projecti/PlayController.java
+++ src/main/java/edu/hust/tu/projecti/PlayController.java
@@ -4,7 +4,6 @@ import edu.hust.tu.projecti.classes.P1QuestionSet;
 import edu.hust.tu.projecti.classes.QuestionContent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -12,11 +11,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PlayController implements Initializable {
-
     @FXML
-    private Label questionField;
+    private Label lQuestionField;
     @FXML
-    private Button answer1, answer2, answer3, answer4;
+    private Button bAnswer1, bAnswer2, bAnswer3, bAnswer4;
     private final P1QuestionSet questionSet = new P1QuestionSet();
 
     private int currentQuestion = 0;
@@ -24,26 +22,27 @@ public class PlayController implements Initializable {
     public void handleAnswer() {
         QuestionContent currentQuestionContent = questionSet.questions[currentQuestion];
         String rightAnswer = currentQuestionContent.answers[currentQuestionContent.rightAnswer];
-        answer1.setOnAction(event -> {
-            if (answer1.getText().equals(rightAnswer))  setQuestion(questionSet.questions[currentQuestion++]);
+        bAnswer1.setOnAction(event -> {
+            if (bAnswer1.getText().equals(rightAnswer))  setQuestion(questionSet.questions[++currentQuestion]);
         });
-        answer2.setOnAction(event -> {
-            if (answer2.getText().equals(rightAnswer))  setQuestion(questionSet.questions[currentQuestion++]);
+        bAnswer2.setOnAction(event -> {
+            if (bAnswer2.getText().equals(rightAnswer))  setQuestion(questionSet.questions[++currentQuestion]);
         });
-        answer3.setOnAction(event -> {
-            if (answer3.getText().equals(rightAnswer))  setQuestion(questionSet.questions[currentQuestion++]);
+        bAnswer3.setOnAction(event -> {
+            if (bAnswer3.getText().equals(rightAnswer))  setQuestion(questionSet.questions[++currentQuestion]);
         });
-        answer4.setOnAction(event -> {
-            if (answer4.getText().equals(rightAnswer))  setQuestion(questionSet.questions[currentQuestion++]);
+        bAnswer4.setOnAction(event -> {
+            if (bAnswer4.getText().equals(rightAnswer))  setQuestion(questionSet.questions[++currentQuestion]);
         });
     }
 
     public void setQuestion(QuestionContent currentQuestion) {
-        questionField.setText(currentQuestion.question);
-        answer1.setText(currentQuestion.answers[0]);
-        answer2.setText(currentQuestion.answers[1]);
-        answer3.setText(currentQuestion.answers[2]);
-        answer4.setText(currentQuestion.answers[3]);
+        lQuestionField.setText(currentQuestion.question);
+        bAnswer1.setText(currentQuestion.answers[0]);
+        bAnswer2.setText(currentQuestion.answers[1]);
+        bAnswer3.setText(currentQuestion.answers[2]);
+        bAnswer4.setText(currentQuestion.answers[3]);
+        handleAnswer();
     }
 
     @Override
@@ -55,9 +54,11 @@ public class PlayController implements Initializable {
                 System.out.println("\t" + a);
             }
             System.out.println("TRUE " + "True Index : " + q.rightAnswer + " Value :" + q.answers[q.rightAnswer] + "\n\n\n");
+
+
         }
         setQuestion(questionSet.questions[currentQuestion]);
-        handleAnswer();
+        //handleAnswer();
     }
 }
 
